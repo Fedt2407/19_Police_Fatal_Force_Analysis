9bb858592233879e996db233a7536f55c106bc19
Fedt2407 <federico.tognetti@gmail.com>
2024-07-26 15:07:39 +0200

    Added deaths by police data structure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 import os
 from dotenv import load_dotenv
+import pandas as pd
 
 # Load environment variables from .env file
 load_dotenv()
@@ -10,8 +11,12 @@ app.secret_key = os.getenv('FLASK_APP_KEY')
 
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def home():
+    data_deaths = pd.read_csv('./static/data/Deaths_by_Police_US.csv', index_col=0, encoding='ISO-8859-1')
+    shape_deaths = data_deaths.shape
+    head_deaths = data_deaths.head().to_html(classes='dataframe', index=False)
+
+    return render_template('index.html', shape_deaths=shape_deaths, head_deaths=head_deaths)
 
 
 if __name__ == '__main__':
